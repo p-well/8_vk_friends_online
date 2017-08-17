ec4e37b616066a6613ac838750d1a779fe834b95
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-17 13:18:26 +0300

    Removed prints

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,7 +1,6 @@
 import vk
 import getpass
-
-APP_ID = 6149210  
+import settings
 
 def get_user_login():
     return input('Your VK login: ')
@@ -11,21 +10,18 @@ def get_user_password():
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
-        app_id=APP_ID,
+        app_id=settings.APP_ID,
         user_login=login,
         user_password=password,
         scope = 2)
 
     api = vk.API(session)
     online_friends_ids = api.friends.getOnline()
-    print(online_friends_ids)
-    print(len(online_friends_ids))
     online_friends_info = api.users.get(user_ids = online_friends_ids, lang = 3)
-    print(online_friends_info)
     return [(friend['first_name'], friend['last_name']) for friend in online_friends_info]
 
 def output_friends_to_console(friends_online):
-    print('\nNow online:\n')
+    print('\nNow online:')
     for friend in friends_online:
         print('{} {}'.format(friend[0], friend[1]))
 
