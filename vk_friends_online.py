c541cecb0b6877f9acb6151e045a7a02a3c4ffe5
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-17 13:17:04 +0300

    working version

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,36 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
+APP_ID = 6149210  
 
 def get_user_login():
-    pass
-
+    return input('Your VK login: ')
 
 def get_user_password():
-    pass
-
+    return getpass.getpass('\nYour VK password: ')
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+        scope = 2)
 
+    api = vk.API(session)
+    online_friends_ids = api.friends.getOnline()
+    print(online_friends_ids)
+    print(len(online_friends_ids))
+    online_friends_info = api.users.get(user_ids = online_friends_ids, lang = 3)
+    print(online_friends_info)
+    return [(friend['first_name'], friend['last_name']) for friend in online_friends_info]
 
 def output_friends_to_console(friends_online):
-    pass
+    print('\nNow online:\n')
+    for friend in friends_online:
+        print('{} {}'.format(friend[0], friend[1]))
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    online_friends_name_lastname = get_online_friends(login, password)
+    output_friends_to_console(online_friends_name_lastname)
